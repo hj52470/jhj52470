8e7a76d44fd2331c981cf37c3c9781df423dba15
hj52470 <jhj33175247@gmail.com>
2025-10-18 02:24:07 +0900

    Update timer.c

--- devices/timer.c
+++ devices/timer.c
@@ -172,6 +172,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
     ticks++;
     thread_tick ();
+   thread_wake_up (ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
